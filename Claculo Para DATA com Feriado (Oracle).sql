0f1d0dfc6fcc963c6bfb5d5fa4d5f60606bb5d8f
Fernando Leal <fernando_leal@id.uff.br>
2020-04-16 17:22:30 -0300

    Calculo para DATA com Feriado

---
+++ Claculo Para DATA com Feriado (Oracle).sql
@@ -0,0 +1,25 @@
+
+
+--DATA PRAZO + 2 dias--
+--Comando SQL (Oracle)
+
+SELECT '1' codAttributeCustomValue , 
+to_char(
+    case when to_char(TO_DATE('{Formulario.campoData}')  +2,'DY', 'nls_language=''BRAZILIAN PORTUGUESE''') = 'S�B' 
+    then TO_DATE('{Formulario.campoData}') +4 else (
+        case when to_char(TO_DATE('{Formulario.campoData}') +2,'DY', 'nls_language=''BRAZILIAN PORTUGUESE''') = 'DOM' 
+        then TO_DATE('{Formulario.campoData}') +4 else TO_DATE('{Formulario.data}') +3 
+        end) 
+    end, 'DD/MM/YYYY') DsAttributeValueName FROM DUAL
+
+
+
+
+--VERIFICA SE DATA E FERIADO
+--Comando SQL (Oracle)
+
+select  CodHoliday CodAttributeCustomValue , DsHolidayTitle,  to_char(DtHoliday,'DD/MM/YYYY') DsAttributeValueName  from wfHOLIDAY 
+where trunc(DtHoliday) = '{Formulario.campoData}'
+
+
+
