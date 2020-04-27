67c18c7e81a2dd357dff81a048ea3cc61eaa5e95
Fernando Leal <fernando_leal@id.uff.br>
2020-04-27 16:44:23 -0300

    moment atualizado

--- Função - Calcular Data com SQL.js
+++ Função - Calcular Data com SQL.js
@@ -1,72 +1,65 @@
-var $dataRealParaPagamento = new Date(), validaData = 0;
-var EFeriado = true;
-
-var _dataRealPg = () => {
-    var data = $dataRealParaPagamento.toLocaleDateString();
-    var d = new Date();
-    var n = d.getHours();
-    var m = d.getMinutes();
-    var s = d.getSeconds();
-    //var dataSimulation = new Date(parseInt(data.split('/')[2]), parseInt(data.split('/')[1]) - 1, parseInt(data.split('/')[0]) + 1);
-    //fonte de dados : FI01 - DATA PRAZO + 3 dias
-    $.ajax({
-        type: "GET",
-        async: false,
-        url: "../api/json/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJnWkF0atIBO57yIS81jGhIvRSzFo0irD7NCVJv8ug7F3xNgRKWIbvA0AkXbT1EHq2A__",
-        data: {
-            inpdata: data.split('/')[2] + '/' + data.split('/')[1] + '/' + data.split('/')[0] + ' ' + n + ':' + m + ':' + s
-        },
-        success: function (response) {
-            if (response.success.length > 0) {
-                var dataRealParaPagamento = response.success[0].txt;
-                _verificaFeriado(dataRealParaPagamento);
-            }
-        }
-    });
-}
-
-
-var _verificaFeriado = (dataRealParaPagamento) => {
-    var validacao = validaData;
-    var teste = dataRealParaPagamento;
-    var dataReal = $dataRealParaPagamento.toLocaleDateString();
-    while (EFeriado) {
-        //FI01 - VERIFICA SE DATA E FERIADO
-        $.ajax({
-            type: "GET",
-            async: false,
-            url: "../api/json/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJqEWLwl9QB5b1ZA-mPBokUDb3D7Z@5WBTPCt9O@AZq1iHRTlcmbZu23jVB2C0aSRUg__",
-            data: {
-                inpdata: dataRealParaPagamento
-            },
-            success: function (response) {
-                if (response.success.length > 0) {
-                    $dataRealParaPagamento = new Date(parseInt(dataReal.split('/')[2]), parseInt(dataReal.split('/')[1]) - 1, parseInt(dataReal.split('/')[0]) + 1);
-                    _dataRealPg();
-                } else {
-                    validaData++;
-                    if(validaData == 3){
-                        var d = new Date();
-                        var n = d.getHours();
-                        var m = d.getMinutes();
-                        var s = d.getSeconds();
-                        EFeriado = false
-                        $('inp:dataInicio').val(teste  + ' ' + n + ':' + m + ':' + s);
-                    } else {
-                        $dataRealParaPagamento = new Date(parseInt(dataReal.split('/')[2]), parseInt(dataReal.split('/')[1]) - 1, parseInt(dataReal.split('/')[0]) + 1);
-                        _dataRealPg();
+var $dtAprovacaoNivel2 = new Date(), validaData = 0;
+        var EFeriado = true;
+                
+        var _dataRealPg = () => {
+            var dtAprovacaoNivel2 = $dtAprovacaoNivel2.toLocaleDateString();
+            var d = new Date();
+            var n = d.getHours();
+            var m = d.getMinutes();
+            var s = d.getSeconds();
+            //var dataSimulation = new Date(parseInt(data.split('/')[2]), parseInt(data.split('/')[1]) - 1, parseInt(data.split('/')[0]) + 1);
+            //fonte de dados : FI01 - DATA PRAZO + 3 dias
+            $.ajax({
+                type: "GET",
+                async: false,
+                url: "../api/json/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJnWkF0atIBO57yIS81jGhIvRSzFo0irD7NCVJv8ug7F3xNgRKWIbvA0AkXbT1EHq2A__",
+                data: {
+                    inpdata: dtAprovacaoNivel2
+                },
+                success: function (response) {
+                    if (response.success.length > 0) {
+                        var dtAprovacaoNivel2 = response.success[0].txt;
+                        _verificaFeriado(dtAprovacaoNivel2);
                     }
                 }
+            });
+        }
+        
+        
+        var _verificaFeriado = (dtAprovacaoNivel2) => {
+            var validacao = validaData;
+            var teste = dtAprovacaoNivel2;
+            var dataRealDePagamento = $dtAprovacaoNivel2.toLocaleDateString();
+            while (EFeriado) {
+                //FI01 - VERIFICA SE DATA E FERIADO
+                $.ajax({
+                    type: "GET",
+                    async: false,
+                    url: "../api/json/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJqEWLwl9QB5b1ZA-mPBokUDb3D7Z@5WBTPCt9O@AZq1iHRTlcmbZu23jVB2C0aSRUg__",
+                    data: {
+                        inpdata: dtAprovacaoNivel2
+                    },
+                    success: function (response) {
+                        if (response.success.length > 0) {
+                            $dtAprovacaoNivel2 = new Date(parseInt(dataRealDePagamento.split('/')[2]), parseInt(dataRealDePagamento.split('/')[1]) - 1, parseInt(dataRealDePagamento.split('/')[0]) + 1);
+                            _dataRealPg();
+                        } else {
+                            validaData++;
+                            if(validaData == 3){
+                                var d = new Date();
+                                var n = d.getHours();
+                                var m = d.getMinutes();
+                                var s = d.getSeconds();
+                                EFeriado = false
+                                $('inp:dataRealDePagamento').val(teste  + ' ' + n + ':' + m + ':' + s);
+                            } else {
+                                $dtAprovacaoNivel2 = new Date(parseInt(dataRealDePagamento.split('/')[2]), parseInt(dataRealDePagamento.split('/')[1]) - 1, parseInt(dataRealDePagamento.split('/')[0]) + 1);
+                                _dataRealPg();
+                            }
+                        }
+                    }
+                });
             }
-        });
-    }
-}
+        }
 
-
-
-
-            
-
-
-
-
+        _dataRealPg();
