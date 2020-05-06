4d3f79e1d70cc924ff9e03169e16b296edbdcd34
Fernando Leal <fernando_leal@id.uff.br>
2020-05-06 12:09:42 -0300

    Atualização de Cód

--- Bloquear - Readonly - Campos.js
+++ Bloquear - Readonly - Campos.js
@@ -1,7 +1,14 @@
 // Readonly no Campo
+
+// Campo bloqueado 
 $('inp:idDoCampo').css('pointer-events', 'none');
 $('inp:idDoCampo').prop('readonly', true);
 $('inp:idDoCampo').attr("tabindex","-1");
+
+//Campo Liberado
+$('inp:idDoCampo').css('pointer-events', 'auto');
+$('inp:idDoCampo').prop('readonly', false);
+$('inp:idDoCampo').attr("tabindex","1");
 
 
 
