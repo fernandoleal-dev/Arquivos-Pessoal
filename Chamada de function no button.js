e726e258a533dd2b3a3c802a00b6e6df5360b9cb
Fernando Leal <fernando_leal@id.uff.br>
2020-05-08 15:08:35 -0300

    OK

--- Chamada de function no button.js
+++ Chamada de function no button.js
@@ -23,3 +23,24 @@ function nomeDaFunction() {
    
   return true;
 }
+
+
+function verificaExistenciaDeAnexos() {
+  var encaminharPlanilha = $('inp:encaminharPlanilhaTrabalhador').val();
+  if (encaminharPlanilha == 'Sim' && $('[name=inpDsFileName]').length == 0) {
+      alert('É necessário anexar a planilha de trabalhadores');
+      return false;
+  }
+  var totalLinhas = $('#tbleProdutos tr').length - 2,
+      result = 0;
+  for (var i = 0; i <= totalLinhas; i++) {
+      if ($('inp:codClasse').eq(i).val() == 34 || $('inp:codClasse').eq(i).val() == 16 || $('inp:codClasse').eq(i).val() == 56) {
+          result++;
+      }
+  }
+  if (result !== 0 && $('[name=inpDsFileName]').length == 0 && $('inp:necessitaAjusteNaProposta').val() == "Não") {
+      alert('É necessário anexar a planilha de trabalhadores');
+      return false;
+  }
+  return true;
+}
